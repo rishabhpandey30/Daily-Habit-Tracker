2653444d84badcf23e5a1158df73f8c21d314437
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-12-08 23:32:24 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api'|| process.env.REACT_APP_BACKEND_URL ;
+const API_URL = 'http://localhost:5000/api'|| 'https://daily-habit-tracker-w80e.onrender.com' ||process.env.REACT_APP_BACKEND_URL ;
 
     const getAuthHeaders = () => {
     const user = JSON.parse(localStorage.getItem('user'));
