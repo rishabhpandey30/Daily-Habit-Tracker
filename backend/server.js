2aa412a079f5b5468fad1b9b91c706bbc561bd68
Rishabh Pandey <115353162+rishabhpandey30@users.noreply.github.com>
2025-12-09 01:29:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(cors({
     "https://habittrackerdev.vercel.app"   // your actual vercel frontend URL
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 })); // Allows cross-origin requests from frontend
 app.options("*", cors());
